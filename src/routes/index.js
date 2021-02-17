dc9c3f0aac2ead1061134d487b1d704af1511cb6
lucaskbr <lucas_klasa@hotmail.com>
2021-02-17 09:56:14 -0300

    fix: index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import WeatherSearchPage from '../pages/WeatherSearchPage';
+import WeatherSearchPage from 'pages/WeatherSearchPage';
 
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={WeatherSearchPage} />
+        <Route exact path="/">
+          <WeatherSearchPage />
+        </Route>
       </Switch>
     </Router>
   );
