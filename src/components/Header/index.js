1f98c62254be4c8b871df6137f47c3b2860e5751
lucaskbr <lucas_klasa@hotmail.com>
2021-02-16 21:22:44 -0300

    feat: add component Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header = () => {
+  return (
+    <Container>
+      <h1>Weather Buddy</h1>
+      <hr width="70%" /> 
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100%;
+  padding: 20px; 
+
+  h1 {
+    font-size: 38px;
+    text-transform: uppercase;
+    text-align: center;
+    font-weight: 600;
+  }
+
+  hr {
+    height: 3px;
+    background: #000;
+  }
+
+`;
